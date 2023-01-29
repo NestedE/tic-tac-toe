a735db55b7ffb069618c95fc0484e2d09f9aa114
Emmanuel Davies <ifioksluvv@gmail.com>
2023-01-29 15:13:55 +0100

    changes made on all files

---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+function openPlayerConfig() {
+	playerConfigOverlayElement.style.display = 'block';
+	backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig() {
+	playerConfigOverlayElement.style.display = 'none';
+	backdropElement.style.display = 'none';
+}
